dbbef2e1c8b96ee3b9016c5decd13a481fc99b56
Hasnain Alahi <hasnainalahiwork@gmail.com>
2026-03-11 16:22:00 +0000

    Wed, Mar 11, 2026, 12:22 PM -04:00

--- footer.js
+++ footer.js
@@ -1,8 +1,15 @@
-function insertFooter(){
-	var htmlToReturn = "<p>Created by Loaf</p>";
-
-  return htmlToReturn;
+// Function to define the footer HTML
+function insertFooter() {
+  return "<p>Created by Loaf</p>";
 }
 
-var footer = document.getElementById("footer");
-footer.innerHTML = insertFooter();
+// Wait for the HTML to be fully loaded before running
+document.addEventListener("DOMContentLoaded", function() {
+  var footerElement = document.getElementById("footer");
+
+  // Safety check: Only run if the 'footer' ID exists on this specific page
+  if (footerElement) {
+    footerElement.innerHTML = insertFooter();
+  }
+});
+
