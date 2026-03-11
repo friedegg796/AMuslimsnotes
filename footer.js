bb052a28aaa0b059d652206d91b03105344f12a9
Hasnain Alahi <hasnainalahiwork@gmail.com>
2026-03-11 16:02:37 +0000

    Wed, Mar 11, 2026, 12:02 PM -04:00

---
+++ footer.js
@@ -0,0 +1,8 @@
+function insertFooter(){
+	var htmlToReturn = "<p>Created by Loaf</p>";
+
+  return htmlToReturn;
+}
+
+var footer = document.getElementById("footer");
+footer.innerHTML = insertFooter();
